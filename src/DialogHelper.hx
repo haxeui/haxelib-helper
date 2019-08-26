b30528327a14f45d287464b6dd03ffdf44c331c7
Ian Harrigan <ianharrigan@hotmail.com>
2019-08-26 08:52:37 +0200

    dont use systools (GTK2 incompatibility)

--- src/DialogHelper.hx
+++ src/DialogHelper.hx
@@ -1,22 +1,32 @@
 package;
+import haxe.ui.core.Screen;
 
 class DialogHelper {
     public function new() {
     }
     
     public static function folder(title:String, message:String):String {
-        #if systools
-        return systools.Dialogs.folder(title, message);
+        #if haxeui_hxwidgets
+        var dialog = new hx.widgets.DirDialog(Screen.instance.frame, title);
+        if (dialog.showModal() == hx.widgets.StandardId.OK) {
+            return dialog.path;
+        }
+        return null;
         #else
         return null;
         #end
     }
     
     public static function message(title:String, message:String, isError:Bool = false) {
-        #if systools
-        return systools.Dialogs.message(title, message, isError);
+        #if haxeui_hxwidgets
+        if (isError == true) {
+            var dialog = new hx.widgets.MessageDialog(Screen.instance.frame, message, title);
+            dialog.showModal();
+        } else {
+            var dialog = new hx.widgets.MessageDialog(Screen.instance.frame, message, title, hx.widgets.styles.MessageDialogStyle.ICON_ERROR);
+            dialog.showModal();
+        }
         #else
-        return null;
         #end
     }
 }
